0c3463c7a43ce1060cc6e9d529f1983a22052645
Catalipsis <Catalipsis@gmail.com>
2016-11-23 11:18:39 +0600

    first commit

---
+++ mnk1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace mnk1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(307, 24);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(773, 579);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(168, 136);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(168, 212);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(168, 176);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown3.TabIndex = 5;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Размер выборки (N)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(24, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Степень полинома";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(26, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Параметр ошибки D";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(24, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Функции:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(27, 52);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "cos(2 * PI * x)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(27, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(106, 20);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "5x^3 + x^2 + 5";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(27, 98);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 20);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "x * sin(2 * PI * x)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 617);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ mnk1/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace mnk1
+{
+    public partial class Form1 : Form
+    {
+        public const int N = 100; // размер выборки
+        public const int D = 2;
+        public const int M = 3;
+        public const int FUNCTION_NUMBER = 1; // 1 -> y = cos(2 * PI * x)
+                                              // 2 -> y = 5x^3 + x^2  + 5
+                                              // 3 -> y = x * sin(2 * PI * x)
+                                              // 4 -> y = 2 * x;
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[0].Name = "Функция";
+            chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            chart1.Series[1].Name = "Выборка";
+            chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[2].Name = "Найденная функция";
+
+            //Отрисовка графика для заданной функции
+            drawFunction(FUNCTION_NUMBER);
+
+            selectionElement[] SelectionTable = new selectionElement[N];
+            for (int i = 0; i < N; i++)
+            {
+                SelectionTable[i] = new selectionElement();
+            }
+
+            //Генерирование выборки
+            createSelection(FUNCTION_NUMBER, SelectionTable);
+
+            //вывод выборки на экран
+            foreach (selectionElement elem in SelectionTable)
+            {
+                chart1.Series[1].Points.AddXY(elem.X, elem.Y);
+            }
+
+            double[,] matrix = new double[M, M];
+            double[] b = new double[M];
+
+            matrix = createMatrix(SelectionTable);
+            b = createRightVector(SelectionTable);
+
+            //Решение СЛАУ методом Гаусса
+            LinearSystem ls = new LinearSystem(matrix, b);
+            double[] w = ls.XVector; //вектор с результатом
+
+            drawResultFunction(w, FUNCTION_NUMBER);
+        }
+
+        //Функция генерирует выборку
+        void createSelection(int functionNumber, selectionElement[] selectionTable)
+        {
+            double xi = 0;
+            double yi = 0;
+            Random r = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                xi = r.NextDouble() * (2 * Math.PI - 0) + 0;
+
+                if (functionNumber == 1)
+                    yi = CosFunction(xi);
+                else if (functionNumber == 2)
+                    yi = PolynomFunction(xi);
+                else if (functionNumber == 3)
+                    yi = SinFunction(xi);
+                else
+                    yi = SimpleFunction(xi);
+                yi += r.NextDouble() * (D - D * (-1)) + D * (-1); //Моделирование ошибки измерения
+
+                selectionTable[i].X = xi;
+                selectionTable[i].Y = yi;
+            }
+        }
+
+        //Функция выводит на экран график заданной функции
+        void drawFunction(int functionNumber)
+        {
+            double y = 0;
+
+            switch (functionNumber)
+            {
+
+                case 1:
+                    for (double x = 0; x < Math.PI * 2; x += Math.PI / 180.0)
+                    {
+                        y = CosFunction(x);
+                        chart1.Series[0].Points.AddXY(x, y);
+
+                    }
+                    break;
+                case 2:
+                    for (double x = 0; x < 15; x++)
+                    {
+                        y = PolynomFunction(x);
+                        chart1.Series[0].Points.AddXY(x, y);
+                    }
+                    break;
+                case 3:
+                    for (double x = 0; x < Math.PI * 2; x += Math.PI / 180.0)
+                    {
+                        y = SinFunction(x);
+                        chart1.Series[0].Points.AddXY(x, y);
+                    }
+                    break;
+                case 4:
+                    for (double x = 0; x < 10; x++)
+                    {
+                        y = SimpleFunction(x);
+                        chart1.Series[0].Points.AddXY(x, y);
+                    }
+                    break;
+            }
+
+        }
+
+        //Функция выводит на экран найденную функцию
+        void drawResultFunction(double[] w, int functionNumber)
+        {
+
+            double y = 0;
+            switch (functionNumber)
+            {
+
+                case 1:
+                    for (double x = 0; x < Math.PI * 2; x += Math.PI / 180.0)
+                    {
+                        y = ResultFunction(w,x);
+                        chart1.Series[2].Points.AddXY(x, y);
+
+                    }
+                    break;
+                case 2:
+                    for (double x = 0; x < 15; x++)
+                    {
+                        y = ResultFunction(w, x);
+                        chart1.Series[2].Points.AddXY(x, y);
+                    }
+                    break;
+                case 3:
+                    for (double x = 0; x < Math.PI * 2; x += Math.PI / 180.0)
+                    {
+                        y = ResultFunction(w, x);
+                        chart1.Series[2].Points.AddXY(x, y);
+                    }
+                    break;
+                case 4:
+                    for (double x = 0; x < 10; x++)
+                    {
+                        y = ResultFunction(w, x);
+                        chart1.Series[2].Points.AddXY(x, y);
+                    }
+                    break;
+            }
+
+        }
+
+
+        double CosFunction(double x) { return Math.Cos(2 * Math.PI * x); }
+        double SinFunction(double x) { return x * Math.Sin(2 * Math.PI * x); }
+        double PolynomFunction(double x) { return 5 * Math.Pow(x, 3) + Math.Pow(x, 2) + 5; }
+        double ResultFunction(double[] w, double x)
+        {
+            double sum = 0;
+            for (int i = 0; i < w.Length; i++)
+            {
+                sum += w[i] * Math.Pow(x, i);
+            }
+            return sum;
+        }
+
+        double SimpleFunction(double x) { return 2 * x; }
+
+        //Создание и заполнение матрицы (A)
+        double[,] createMatrix(selectionElement[] selectionTable)
+        {
+            double sum = 0;
+            double[,] matrix = new double[M, M];
+
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < M; j++)
+                {
+                    for (int g = 0; g < selectionTable.Length; g++)
+                    {
+                        sum += Math.Pow(selectionTable[g].X, i + j);
+                    }
+                    matrix[i, j] = sum;
+                    sum = 0;
+                }
+            return matrix;
+        }
+
+        //Создание и заполнение вектора правых частей (b)
+        double[] createRightVector(selectionElement[] selectionTable)
+        {
+            double sum = 0;
+            double[] b = new double[M];
+            for (int i = 0; i < M; i++)
+            {
+                for (int k = 0; k < selectionTable.Length; k++)
+                {
+                    sum += selectionTable[k].Y * Math.Pow(selectionTable[k].X, i);
+                }
+
+                b[i] = sum;
+                sum = 0;
+            }
+            return b;
+        }
+
+    }
+
+
+    //Класс Элемент выборки
+    class selectionElement
+    {
+        double x;
+        double y;
+
+        public selectionElement() { x = 0; y = 0; }
+        public selectionElement(double theX, double theY) { x = theX; y = theY; }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
