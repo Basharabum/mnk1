799a58a05f0f7a4966a24c57f45851fd9f59fc1b
Catalipsis <Catalipsis@gmail.com>
2016-11-29 00:55:44 +0600

    28comp

--- mnk1/Form1.Designer.cs
+++ mnk1/Form1.Designer.cs
@@ -44,6 +44,10 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
@@ -186,11 +190,63 @@
             this.radioButton3.Text = "x * sin(2 * PI * x)";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(28, 516);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Функция";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(28, 539);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "Выборка";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(28, 562);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(131, 17);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "Найденный полином";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 497);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Отображать:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1099, 617);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -226,6 +282,10 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- mnk1/Form1.cs
+++ mnk1/Form1.cs
@@ -14,12 +14,16 @@ namespace mnk1
     public partial class Form1 : Form
     {
         public const int N = 100; // размер выборки
-        public const int D = 2;
-        public const int M = 3;
+        public const int D = 1;
+        public const int M = 30;
+        public const int MISTAKE_TYPE = 1;    // 1 -> с равномерным распределением
+                                              // 2 -> с нормальным распределением
+
         public const int FUNCTION_NUMBER = 1; // 1 -> y = cos(2 * PI * x)
                                               // 2 -> y = 5x^3 + x^2  + 5
                                               // 3 -> y = x * sin(2 * PI * x)
-                                              // 4 -> y = 2 * x;
+                                              // 4 -> y = 2 * x; простая функция
+
         public Form1()
         {
             InitializeComponent();
@@ -33,6 +37,7 @@ namespace mnk1
             //Отрисовка графика для заданной функции
             drawFunction(FUNCTION_NUMBER);
 
+            //Создание массива под выборку
             selectionElement[] SelectionTable = new selectionElement[N];
             for (int i = 0; i < N; i++)
             {
@@ -51,13 +56,16 @@ namespace mnk1
             double[,] matrix = new double[M, M];
             double[] b = new double[M];
 
+            //создание матрицы на основе выборки
             matrix = createMatrix(SelectionTable);
+            //создание вектора правых частей
             b = createRightVector(SelectionTable);
 
             //Решение СЛАУ методом Гаусса
             LinearSystem ls = new LinearSystem(matrix, b);
             double[] w = ls.XVector; //вектор с результатом
 
+            //Вывод на экран найденного полинома
             drawResultFunction(w, FUNCTION_NUMBER);
         }
 
@@ -72,19 +80,45 @@ namespace mnk1
             {
                 xi = r.NextDouble() * (2 * Math.PI - 0) + 0;
 
-                if (functionNumber == 1)
-                    yi = CosFunction(xi);
-                else if (functionNumber == 2)
-                    yi = PolynomFunction(xi);
-                else if (functionNumber == 3)
-                    yi = SinFunction(xi);
-                else
-                    yi = SimpleFunction(xi);
-                yi += r.NextDouble() * (D - D * (-1)) + D * (-1); //Моделирование ошибки измерения
+                //Вычисление значения функции
+                switch (functionNumber)
+                {
+                    case 1:
+                        yi = CosFunction(xi);
+                        break;
+                    case 2:
+                        yi = PolynomFunction(xi);
+                        break;
+                    case 3:
+                        yi = SinFunction(xi);
+                        break;
+                    case 4:
+                        yi = SimpleFunction(xi);
+                        break;
+                }
 
+                //Добавление ошибки
+                if (MISTAKE_TYPE == 1)
+                {
+                    //min + r.NextDouble() * (max - min);
+                    yi += D * (-1) + r.NextDouble() * (D - D * (-1)); //Моделирование ошибки измерения с равномерным распределением на отрезке [-D,D]
+                }
+                else
+                {
+                    yi += r.NextNormal() * D; //Моделирование ошибки измерения с нормальным распределением на отрезке (0,D)
+                }
                 selectionTable[i].X = xi;
                 selectionTable[i].Y = yi;
             }
+        }
+
+        double calculateError(double[] w, selectionElement[] SelectionTable)
+        {
+            double resultSum = 0;
+            for (int i = 0; i < N; i++)
+                resultSum += Math.Pow(SelectionTable[i].Y - ResultFunction(w, SelectionTable[i].X), 2);
+
+            return resultSum /= 2;
         }
 
         //Функция выводит на экран график заданной функции
@@ -94,8 +128,8 @@ namespace mnk1
 
             switch (functionNumber)
             {
-
                 case 1:
+                    // y = cos(2 * PI * x)
                     for (double x = 0; x < Math.PI * 2; x += Math.PI / 180.0)
                     {
                         y = CosFunction(x);
@@ -104,6 +138,7 @@ namespace mnk1
                     }
                     break;
                 case 2:
+                    //y = 5x^3 + x^2  + 5
                     for (double x = 0; x < 15; x++)
                     {
                         y = PolynomFunction(x);
@@ -111,6 +146,7 @@ namespace mnk1
                     }
                     break;
                 case 3:
+                    //y = x * sin(2 * PI * x)
                     for (double x = 0; x < Math.PI * 2; x += Math.PI / 180.0)
                     {
                         y = SinFunction(x);
@@ -118,6 +154,7 @@ namespace mnk1
                     }
                     break;
                 case 4:
+                    //y = 2 * x;
                     for (double x = 0; x < 10; x++)
                     {
                         y = SimpleFunction(x);
@@ -128,10 +165,9 @@ namespace mnk1
 
         }
 
-        //Функция выводит на экран найденную функцию
+        //Функция выводит на экран найденный полином
         void drawResultFunction(double[] w, int functionNumber)
         {
-
             double y = 0;
             switch (functionNumber)
             {
@@ -139,9 +175,8 @@ namespace mnk1
                 case 1:
                     for (double x = 0; x < Math.PI * 2; x += Math.PI / 180.0)
                     {
-                        y = ResultFunction(w,x);
+                        y = ResultFunction(w, x);
                         chart1.Series[2].Points.AddXY(x, y);
-
                     }
                     break;
                 case 2:
@@ -173,6 +208,7 @@ namespace mnk1
         double CosFunction(double x) { return Math.Cos(2 * Math.PI * x); }
         double SinFunction(double x) { return x * Math.Sin(2 * Math.PI * x); }
         double PolynomFunction(double x) { return 5 * Math.Pow(x, 3) + Math.Pow(x, 2) + 5; }
+
         double ResultFunction(double[] w, double x)
         {
             double sum = 0;
@@ -182,7 +218,6 @@ namespace mnk1
             }
             return sum;
         }
-
         double SimpleFunction(double x) { return 2 * x; }
 
         //Создание и заполнение матрицы (A)
@@ -222,6 +257,32 @@ namespace mnk1
             return b;
         }
 
+        //Чекбокс отображения функции
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                chart1.Series[0].Enabled = true;
+            else
+                chart1.Series[0].Enabled = false;
+        }
+
+        //Чекбокс отображения выборки
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                chart1.Series[1].Enabled = true;
+            else
+                chart1.Series[1].Enabled = false;
+        }
+
+        //Чекбокс отображения найденного полинома
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+                chart1.Series[2].Enabled = true;
+            else
+                chart1.Series[2].Enabled = false;
+        }
     }
 
 
@@ -243,6 +304,47 @@ namespace mnk1
         {
             get { return y; }
             set { y = value; }
+        }
+    }
+
+    // Преобразование Бокса — Мюллера для получения нормально распределенной ошибки
+
+    static class RandomExtension
+    {
+        private static bool haveReadyValue;
+        private static double readyValue;
+
+        public static double NextNormal(this Random rand)
+        {
+            double x = 0;
+            double y = 0;
+            double s = 0;
+
+            //Если уже есть сохраненная готовая величина, удовлетворяющая стандартному нормальному распределению
+            if (haveReadyValue)
+            {
+                haveReadyValue = false;
+                return readyValue;
+            }
+            else
+            {
+                do
+                {
+                    //генерация x и y — независимых случайных величин, равномерно распределённых на отрезке [-1,1]
+                    x = 2 * rand.NextDouble() - 1;
+                    y = 2 * rand.NextDouble() - 1;
+                    //Вычисление s по формуле: s = x^2 + y^2
+                    s = x * x + y * y;
+                } while (s > 1 || s == 0); //если s > 1 или s = 0, то значения x и y следует сгенерировать заново
+
+                double root = Math.Sqrt(-2 * Math.Log(s) / s);
+
+                //сохраняем одну из величин, для последующих вызовов
+                readyValue = y * root;
+                haveReadyValue = true;
+
+                return x * root;
+            }
         }
     }
 }
